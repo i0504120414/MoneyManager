8643b98d3d05662f57535820c34ab34e7171047a
genzelisrael-ui <i0504120414@gmail.com>
2025-12-04 23:12:10 +0200

    self

--- src/patches/puppeteerPatchEarly.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-/**
- * Entry point script that ensures Puppeteer is patched before any imports
- * This script should be called instead of directly calling testBankConnection.js
- */
-
-import './puppeteerPatchEarly.js';
-import '../scripts/testBankConnection.js';
